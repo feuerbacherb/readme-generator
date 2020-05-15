c431b916d5be39956bc8bcd5ae43cec5899f4fcc
Brent F <feuerbacherb@gmail.com>
2020-05-15 10:19:15 -0500

    added questions to develop

--- index.js
+++ index.js
@@ -1,16 +1,71 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
 
-];
+const promptUser = () => {
+   return inquirer.prompt([
+      {
+         type: 'input',
+         name: 'title',
+         message: 'What is the title of your project?'
+      },
+      {
+         type: 'input',
+         name: 'description',
+         message: 'Type in a description of your project: (REQUIRED)'
+      },
+      {
+         type: 'input',
+         name: 'installation',
+         message: 'Type instructions for how to install your project: (REQUIRED)'
+      },
+      {
+         type: 'input',
+         name: 'usage',
+         message: 'Type in a description of how the user would use your project: (REQUIRED)'
+      },
+      {
+         type: 'list',
+         name: 'license',
+         message: 'Choose a license to include in the readme.md:',
+         choices: ['Apache License 2.0', 'GNU GPLv3', 'MIT', 'Mozilla Public License 2.0', 'The Unlicense']
+      },
+      {
+         type: 'input',
+         name: 'contributions',
+         message: 'Enter guidelines for those who wish to contribute to your project:'
+      },
+      {
+         type: 'input',
+         name: 'tests',
+         message: 'Enter how to test your project:'
+      },
+      {
+         type: 'input',
+         name: 'email',
+         message: 'Enter your email address:'
+      },
+      {
+         type: 'input',
+         name: 'github',
+         message: 'Enter your GitHub username:'
+      }
+   ]);
+};
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+promptUser().then(answers => console.log(answers));
 
-// function to initialize program
-function init() {
+// // array of questions for user
+// const questions = [
 
-}
+// ];
 
-// function call to initialize program
-init();
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
+
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
